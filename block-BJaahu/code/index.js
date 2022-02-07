f7a664cefb75e0d2af57d152a1a387ae7b7c2ca1
Sanaanju <sanaanjum1944@gmail.com>
2022-02-07 13:08:45 +0530

    object

--- block-BJaahu/code/index.js
+++ block-BJaahu/code/index.js
@@ -1,96 +1,72 @@
-/*### For single question we need the following data and methods:
-- Data:
-  - `title` (title of the question)
-  - `options` (array of options)
-  - `correctAnswerIndex` (index of the correct option)
-- Methods:
-  - `isAnswerCorrect` (will accept the index and returns `true` or `false` based on the answer is correct or not)
-  - `getCorrectAnswer` (will return the correct answer of the quiz when the function is called)
-### Create the object using the following ways
-For each different ways of creating object write different solutions.
-- Prototypal pattern of object creation (Put methods inside an object and create object using Object.create)
-- Pseudoclassical Pattern (Put methods inside F.prototype and use `new` to call function)
-- Create using class
-- Write test by creating two objects also test both methods.*/
+
+// Prototypal pattern
 
 
-//Prototyple pattern
-let questionMethods={
-
-    isAnswerCorrect:function(index){
-       return index===this.correctAnswerIndex;
+let questionMethods = {
+    isAnswerCorrect: function(ans) {
+        return this.correctAnswer === ans
     },
-    getCorrectAnswer:function(){
-       return this.options[this.correctAnswerIndex];
+    getCorrectAnswer: function() {
+        return this.options[this.correctAnswer];
     }
 }
-}
-function createques(title,options,correctAnswerIndex){
-
-    let ques=Object.create(quesMethods);
-    ques.title=title;
-    ques.options=options;
-    ques.correctAnswerIndex=correctAnswerIndex;
 
 
+function Question(title,options, ans) {
+    let obj = Object.create(questionMethods);
+    obj.title = title;
+    obj.options = options;
+    obj.correctAnswer = ans;
 
-    return ques;
+    return obj;
 }
 
-//Pseudoclassical Pattern
+
+// Pseudoclassical Pattern
 
 
-function question (title,options,correctAnswerIndex){
-
- 
-    this.title=title;
-    this.options=options;
-    this.correctAnswerIndex=correctAnswerIndex;
-
-
-
-
+function Question(title,options, ans) {
+    this.title = title;
+    this.options = options;
+    this.correctAnswer = ans;
 }
- question.prototype={
-        isAnswerCorrect:function(index){
-            return index===this.correctAnswerIndex;
-       },
-        getCorrectAnswer:function(){
-            return this.options[this.correctAnswerIndex];
-        }
+
+Question.prototype = {
+    isAnswerCorrect: function(ans) {
+        return this.correctAnswer === ans
+    },
+    getCorrectAnswer: function() {
+        return this.options[this.correctAnswer];
     }
-
-}
-
-//class pattern
-class question{
-
-
-   constructor(title,options,correctAnswerIndex){
-
- 
-       this.title=title;
-       this.options=options;
-       this.correctAnswerIndex=correctAnswerIndex;
-   }
-   isAnswerCorrect(index){
-       return index===this.correctAnswerIndex;
-   }
-   getCorrectAnswer(){
-       return this.options[this.correctAnswerIndex];
-   }
-
-
 }
 
 
-let firstquestion = new question(
-   'Where is the capital of Jordan',
-   ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
-   1
- );
- let secondquestion =  new question(
-   'Where is the capital of Jamaica',
-   ['Tashkent', 'Amaan', 'Kingston', 'Nairobi'],
-   2
- );
+
+// Class pattern
+
+class Question {
+    constructor(title,options, ans) {
+        this.title = title;
+        this.options = options;
+        this.correctAnswer = ans;
+    }
+    isAnswerCorrect(ans) {
+        return this.correctAnswer === ans
+    }
+    getCorrectAnswer() {
+        return this.options[this.correctAnswer];
+    }
+}
+
+
+let firstQuestion = new Question(
+    'Where is the capital of Jordan',
+    ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
+    1
+);
+
+let secondQuestion = new Question(
+    'Where is the capital of Jamaica',
+    ['Tashkent', 'Amaan', 'Kingston', 'Nairobi'],
+    2
+);
